c8b2e6119886f06a4fd494e011685c6a7ea74b35
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-10-11 22:01:00 +0500

    v13 stuf

--- src/Events/message/messageUpdate.js
+++ src/Events/message/messageUpdate.js
@@ -16,14 +16,14 @@ module.exports = class extends Event {
 				`**❯ Message ID:** ${old.id}`,
 				`**❯ Channel:** ${old.channel}`,
 				`**❯ Author:** ${old.author.tag} (${old.author.id})`
-			])
+			].join('\n'))
 			.setURL(old.url)
 			.splitFields(diffWordsWithSpace(escapeMarkdown(old.content), escapeMarkdown(message.content))
 				.map(result => result.added ? `**${result.value}**` : result.removed ? `~~${result.value}~~` : result.value)
 				.join(' '));
 
 		const channel = message.guild.channels.cache.find(ch => ch.name === 'testing');
-		if (channel) channel.send(embed);
+		if (channel) channel.send({ embeds: [embed] });
 	}
 
 };
