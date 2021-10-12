afa4e7f755b3251f97610578c59e9b7437e3aa01
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-10-12 17:21:19 +0500

    v13 moment

--- src/Events/message/messageDelete.js
+++ src/Events/message/messageDelete.js
@@ -21,7 +21,7 @@ module.exports = class extends Event {
 		}
 
 		const channel = message.guild.channels.cache.find(ch => ch.name === 'testing' && ch.type === 'GUILD_TEXT');
-		if (channel) channel.send(embed);
+		if (channel) channel.send({ embeds: [embed] })
 	}
 
 };
